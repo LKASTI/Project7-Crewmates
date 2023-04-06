de91054027e97690765f0006c9d99a9043c1862c
Landin Kasti <lkastiman@gmail.com>
2023-04-06 14:03:09 -0500

    added core styling for crew creation and editing pages

--- src/components/Editing.jsx
+++ src/components/Editing.jsx
@@ -2,12 +2,18 @@ import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 import { supabase } from "../client"
 import "./editing.css"
+import wookie from "../assets/wookie.webp"
+import human from "../assets/human.png"
+import twilek from "../assets/twilek.jpg"
+import droid from "../assets/Droid.webp"
+import empty from "../assets/x-mark.png"
 
 const Editing = ({crewMembers}) => {
     const {id} = useParams()
     const [name, setName] = useState()
     const [species, setSpecies] = useState()
     const [role, setRole] = useState()
+    const [memberImage, setMemberImage] = useState()
 
     useEffect(() => {
 
@@ -24,18 +30,57 @@ const Editing = ({crewMembers}) => {
         }
 
     }, [crewMembers])
+
+    useEffect(() => {
+        switch(species)
+        {
+            case "Human":
+                setMemberImage(human)
+                break;
+            case "Droid":
+                setMemberImage(droid)
+                break;
+            case "Wookie":
+                setMemberImage(wookie)
+                break;
+            case "Twilek":
+                setMemberImage(twilek)
+                break;
+            default:
+                setMemberImage(null)
+        }
+    }, [species])
     
     const handleNameChange = (e) => {
         const newName = e.target.value
         setName(newName)
     }
 
-    const handleSpeciesChange = (e) => {
+    const handleSpeciesSelect = (e) => {
         const newSpecies = e.target.value
+
         setSpecies(newSpecies)
+
+        switch(newSpecies)
+        {
+            case "Human":
+                setMemberImage(human)
+                break;
+            case "Droid":
+                setMemberImage(droid)
+                break;
+            case "Wookie":
+                setMemberImage(wookie)
+                break;
+            case "Twilek":
+                setMemberImage(twilek)
+                break;
+            default:
+                setMemberImage(null)
+        }
     }
 
-    const handleRoleChange = (e) => {
+    const handleRoleSelect = (e) => {
         const newRole = e.target.value
         setRole(newRole)
     }
@@ -44,7 +89,7 @@ const Editing = ({crewMembers}) => {
         e.preventDefault()
         const member = {name: `${name}`, species: `${species}`, role: `${role}`}
 
-        if(!(member.name && member.species && member.role))
+        if(!(member.name && member.species && member.role) || (member.species === "--Choose a Species--" || member.role === "--Choose a Role--"))
         {
             alert("Please fill in all fields")
             return
@@ -71,21 +116,40 @@ const Editing = ({crewMembers}) => {
     return(
         <div className="editing-page">
             {crewMembers &&
-                <form onSubmit={updateMember}>
-                    <label>Name</label><br/>
-                    <input id="name" name="name" type="text" value={name} onChange={handleNameChange}/><br/><br/>
+                <form className="editing-form" onSubmit={updateMember}>
+                    <div>
+                        <label>Name</label><br/>
+                        <input id="name" name="name" type="text" value={name} onChange={handleNameChange}/><br/><br/>
+                    </div>
+                    <div>
+                        <label>Species</label><br/>
+                        <select value={species} onChange={handleSpeciesSelect}>
+                            <option>--Choose a Species--</option>
+                            <option>Human</option>
+                            <option>Droid</option>
+                            <option>Wookie</option>
+                            <option>Twilek</option>
+                        </select><br/><br/>
+                    </div>
+                    <div>
+                        <label>Role</label><br/>
+                        <select value={role} onChange={handleRoleSelect}>
+                            <option>--Choose a Role--</option>
+                            <option>Pilot</option>
+                            <option>Co-pilot</option>
+                            <option>Gunner</option>
+                            <option>Medic</option>
+                            <option>Technician</option>
+                        </select><br/><br/>
+                    </div>
 
-                    <label>Species</label><br/>
-                    <input id="species" name="species" type="text" value={species} onChange={handleSpeciesChange}/><br/><br/>
-
-                    <label>Role</label><br/>
-                    <input id="role" name="role" type="text" value={role} onChange={handleRoleChange}/><br/><br/>
-
-                    <input type="submit" value="Submit" />
+                    <input id="submit" type="submit" value="Submit" />
                     <button onClick={deleteMember} className="deleteButton">Delete</button>
                 </form>
-                
             }
+            <div className="editing-img">
+                <img alt="Select a Species" style={{display: memberImage? "block" : "none"}} src={memberImage}/>
+            </div>
         </div>
     )
 }
--- src/pages/Creation.jsx
+++ src/pages/Creation.jsx
@@ -1,23 +1,54 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import {supabase} from "../client"
 import "./creation.css"
+import wookie from "../assets/wookie.webp"
+import human from "../assets/human.png"
+import twilek from "../assets/twilek.jpg"
+import droid from "../assets/Droid.webp"
+import empty from "../assets/x-mark.png"
 
 const Creation = () => {
     const [name, setName] = useState("")
     const [species, setSpecies] = useState("")
     const [role, setRole] = useState("")
+    const [memberImage, setMemberImage] = useState()
+
+    useEffect(() => {
+        setMemberImage("")
+    }, [])
 
     const handleNameChange = (e) => {
         const newName = e.target.value
         setName(newName)
     }
 
-    const handleSpeciesChange = (e) => {
+    const handleSpeciesSelect = (e) => {
         const newSpecies = e.target.value
+
         setSpecies(newSpecies)
+
+        // console.log(human)
+
+        switch(newSpecies)
+        {
+            case "Human":
+                setMemberImage(human)
+                break;
+            case "Droid":
+                setMemberImage(droid)
+                break;
+            case "Wookie":
+                setMemberImage(wookie)
+                break;
+            case "Twilek":
+                setMemberImage(twilek)
+                break;
+            default:
+                setMemberImage(null)
+        }
     }
 
-    const handleRoleChange = (e) => {
+    const handleRoleSelect = (e) => {
         const newRole = e.target.value
         setRole(newRole)
     }
@@ -44,20 +75,47 @@ const Creation = () => {
         window.location = "/gallery"
     }
 
+    const setImage = () => {
+
+    }
+
     return(
         <div className="creation-page">
-            <form onSubmit={createMember}>
-                <label>Name</label><br/>
-                <input id="name" name="name" type="text" value={name} onChange={handleNameChange}/><br/><br/>
+            <form className="creation-form" onSubmit={createMember}>
+                <div>
+                    <label>Name</label><br/>
+                    <input id="name" name="name" type="text" value={name} onChange={handleNameChange}/><br/>
+                </div>
+                <div>
+                    <label>Species</label><br/>
+                    {/* <input id="species" name="species" type="text" value={species} onChange={handleSpeciesChange}/><br/><br/> */}
+                    <select onChange={handleSpeciesSelect}>
+                        <option>--Choose a Species--</option>
+                        <option>Human</option>
+                        <option>Droid</option>
+                        <option>Wookie</option>
+                        <option>Twilek</option>
+                    </select>
+                </div>
 
-                <label>Species</label><br/>
-                <input id="species" name="species" type="text" value={species} onChange={handleSpeciesChange}/><br/><br/>
+                <div>
+                    <label>Role</label><br/>
+                    {/* <input id="role" name="role" type="text" value={role} onChange={handleRoleChange}/><br/><br/> */}
+                    <select onChange={handleRoleSelect}>
+                        <option>--Choose a Role--</option>
+                        <option>Pilot</option>
+                        <option>Co-pilot</option>
+                        <option>Gunner</option>
+                        <option>Medic</option>
+                        <option>Technician</option>
+                    </select>
+                </div>
 
-                <label>Role</label><br/>
-                <input id="role" name="role" type="text" value={role} onChange={handleRoleChange}/><br/><br/>
-
-                <input type="submit" value="Submit" />
+                <input id="submit" type="submit" value="Enlist" />
             </form>
+            <div className="creation-img">
+                <img alt="Select a Species" style={{display: memberImage? "block" : "none"}} src={memberImage}/>
+            </div>
         </div>
     )
 }
