96a87a314d0dd3f08552708081a95749ef1d2018
Landin Kasti <lkastiman@gmail.com>
2023-04-06 10:24:59 -0500

    added core styling for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,14 @@ import "./home.css"
 const Home = () => {
 
     return(
-        <>Home</>
+        <div className="home">
+            <div>
+                <h1>Star<br/>Wars</h1>
+                <p>You're the captain of a Dreadnaught starship!</p>
+                <p>Choose and create crew members to aid you.</p>
+                <p>What do you want your crew to look like?</p>
+            </div>
+        </div>
     )
 }
 
