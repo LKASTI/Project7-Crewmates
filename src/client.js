707af0164fb25de251dd2fce9e69c73fffe8fb5a
LKASTI <89228626+LKASTI@users.noreply.github.com>
2023-09-21 11:22:34 -0500

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,6 +1,1 @@
-import {createClient} from "@supabase/supabase-js"
 
-const URL = "https://rkokuzqnncmghnvugabd.supabase.co"
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrb2t1enFubmNtZ2hudnVnYWJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA2NTYxNTksImV4cCI6MTk5NjIzMjE1OX0.boNPxcCnOaHamZtk9qAQFnEVzunC7AuRmwoZpENY5lk"
-
-export const supabase = createClient(URL, API_KEY)
